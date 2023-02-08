079789b1d936057c440219a4464f8b7ddbefdf38
IGreisenI <kstjepan4@gmail.com>
2023-02-08 10:36:48 +0100

    Add character fields and base UI for the window.

--- Assets/3_Scripts/Editor/CharacterImportWindow.cs
+++ Assets/3_Scripts/Editor/CharacterImportWindow.cs
@@ -5,4 +5,37 @@ using UnityEditor;
 
 public class CharacterImportWindow : EditorWindow
 {
+    private string characterName;
+    private int characterPrice;
+    private GameObject characterModel;
+    private Sprite characterSprite;
+
+    [MenuItem("Window/CharacterImport")]
+    static void ShowWindow()
+    {
+        GetWindow(typeof(CharacterImportWindow));
+    }
+
+    private void DrawUI()
+    {
+        characterName = EditorGUILayout.TextField("Character Name", characterName);
+        characterPrice = EditorGUILayout.IntField("Character Speed", characterPrice);
+        characterModel = (GameObject)EditorGUILayout.ObjectField("Character Model", characterModel, typeof(GameObject), false);
+        characterSprite = (Sprite)EditorGUILayout.ObjectField("Character Sprite", characterSprite, typeof(Sprite), false);
+    }
+
+    private void OnGUI()
+    {
+        DrawUI();
+        if (GUILayout.Button("Import"))
+        {
+            ImportCharacter();
+        }
+    }
+
+    private void ImportCharacter()
+    {
+
+    }
 }
+ 
