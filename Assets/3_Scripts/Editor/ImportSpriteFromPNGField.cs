afe4ce52b4628f6f4c55b03be5bab6deb5294a6e
IGreisenI <kstjepan4@gmail.com>
2023-02-08 15:43:34 +0100

    Add logic for importing PNG as a sprite by adding ImportAsSprite function which returns a Texture2D

--- Assets/3_Scripts/Editor/ImportSpriteFromPNGField.cs
+++ Assets/3_Scripts/Editor/ImportSpriteFromPNGField.cs
@@ -16,16 +16,38 @@ public class ImportSpriteFromPNGField
             if (GUILayout.Button("Select PNG", GUILayout.Width(100)))
             {
                 path = EditorUtility.OpenFilePanel(fieldName, "", "png");
+                ImportAsSprite();
             }
         }
     }
 
-    public static void ImportAsSprite()
+    public static Texture2D ImportAsSprite()
     {
         if (!string.IsNullOrEmpty(path))
         {
-            FileUtil.CopyFileOrDirectory(path, $"Assets/1_Graphics/Store/{Path.GetFileName(path)}");
+            string destinationPath = $"Assets/1_Graphics/Store/{Path.GetFileName(path)}";
+
+            FileUtil.CopyFileOrDirectory(path, destinationPath);
             AssetDatabase.Refresh();
+
+            TextureImporter importer = AssetImporter.GetAtPath(destinationPath) as TextureImporter;
+
+            if (importer != null)
+            {
+                importer.textureType = TextureImporterType.Sprite;
+
+                // Save the changes to the texture asset
+                AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
+            }
+            else
+            {
+                Debug.LogError($"Texture not found at path: {destinationPath}");
+            }
+
+            return (Texture2D)AssetDatabase.LoadAssetAtPath(destinationPath, typeof(Texture2D));
         }
+
+        Debug.LogError("Path provided is empty");
+        return null;
     }
 }
