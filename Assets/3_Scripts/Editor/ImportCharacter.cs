76e4313ef273585c457c7f1a622c1f9c11f0ed06
IGreisenI <kstjepan4@gmail.com>
2023-02-09 17:41:37 +0100

    Create ImportCharacter Scriptable object for settings and creating a store item from the character information.

---
+++ Assets/3_Scripts/Editor/ImportCharacter.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ImportCharacterSettings", menuName = "ImportSettings/CharacterImport")]
+public class ImportCharacter : ScriptableObject
+{
+    [SerializeField] private AnimatorController controller;
+    [SerializeField] private float colliderRadius;
+    [SerializeField] private float colliderHeight;
+
+    public void CreateStoreItem(string characterName, int characterPrice, Object model, Sprite icon)
+    {
+        StoreItem storeItem = ScriptableObject.CreateInstance<StoreItem>();
+        storeItem.Name = characterName;
+        storeItem.Price = characterPrice;
+        storeItem.Icon = icon;
+
+        GameObject prefab = (GameObject)PrefabUtility.InstantiatePrefab(model);
+        CapsuleCollider collider = prefab.AddComponent<CapsuleCollider>();
+        collider.radius = colliderRadius;
+        collider.height = colliderHeight;
+        prefab.GetComponent<Animator>().runtimeAnimatorController = controller;
+        prefab.GetComponent<Animator>().applyRootMotion = false;
+        prefab.GetComponent<Animator>().cullingMode = AnimatorCullingMode.AlwaysAnimate;
+
+        PrefabUtility.UnpackPrefabInstance(prefab, PrefabUnpackMode.OutermostRoot, InteractionMode.AutomatedAction);
+
+        storeItem.Prefab = PrefabUtility.SaveAsPrefabAsset(prefab, $"Assets/2_Prefabs/{prefab.name}.prefab");
+        
+        DestroyImmediate(prefab.gameObject, false);
+
+        AssetDatabase.CreateAsset(storeItem, $"Assets/4_ScriptableObject/{characterName}SO.asset");
+        AssetDatabase.SaveAssets();
+
+        Store.Instance.StoreItems.Add(storeItem);
+    }
+}
