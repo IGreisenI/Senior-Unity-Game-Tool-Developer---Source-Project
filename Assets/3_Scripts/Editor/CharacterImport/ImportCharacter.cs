8c5c60cee7996f5f859a44954a93d23b2f3e7c5b
IGreisenI <kstjepan4@gmail.com>
2023-02-10 12:22:54 +0100

    Refactor ImportCharacter class and add functions
    Move creation of the SO asset to CreateStoreItem function. Move creation of the prefab to it's own function [CreatePrefabForCharacter] to be reused.
    
    Add ReimportTexture and RepimportModel functions for importing new assets.
    
    Add RemoveCharacter for removing the StoreItem object.

--- Assets/3_Scripts/Editor/CharacterImport/ImportCharacter.cs
+++ Assets/3_Scripts/Editor/CharacterImport/ImportCharacter.cs
@@ -7,34 +7,62 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ImportCharacterSettings", menuName = "ImportSettings/CharacterImport")]
 public class ImportCharacter : ScriptableObject
 {
-    [SerializeField] private AnimatorController controller;
+    [SerializeField] private AnimatorController animatorController;
     [SerializeField] private float colliderRadius;
     [SerializeField] private float colliderHeight;
 
     public void CreateStoreItem(string characterName, int characterPrice, Object model, Sprite icon)
     {
         StoreItem storeItem = ScriptableObject.CreateInstance<StoreItem>();
+        storeItem.Id = AssetDatabase.FindAssets("t:" + typeof(StoreItem)).Length;
         storeItem.Name = characterName;
         storeItem.Price = characterPrice;
         storeItem.Icon = icon;
+
+        AssetDatabase.CreateAsset(storeItem, $"Assets/4_ScriptableObject/{characterName}SO.asset");
+        AssetDatabase.SaveAssets();
+
+        CreatePrefabForCharacter(storeItem, model);
+
+        Store.Instance.StoreItems.Add(storeItem);
+    }
+
+    private void CreatePrefabForCharacter(StoreItem storeItem, Object model)
+    {
+        if (storeItem.Prefab)
+        {
+            AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(storeItem.Prefab));
+            storeItem.Prefab = null;
+        }
 
         GameObject prefab = (GameObject)PrefabUtility.InstantiatePrefab(model);
         CapsuleCollider collider = prefab.AddComponent<CapsuleCollider>();
         collider.radius = colliderRadius;
         collider.height = colliderHeight;
-        prefab.GetComponent<Animator>().runtimeAnimatorController = controller;
+        prefab.GetComponent<Animator>().runtimeAnimatorController = animatorController;
         prefab.GetComponent<Animator>().applyRootMotion = false;
         prefab.GetComponent<Animator>().cullingMode = AnimatorCullingMode.AlwaysAnimate;
 
         PrefabUtility.UnpackPrefabInstance(prefab, PrefabUnpackMode.OutermostRoot, InteractionMode.AutomatedAction);
 
         storeItem.Prefab = PrefabUtility.SaveAsPrefabAsset(prefab, $"Assets/2_Prefabs/{prefab.name}.prefab");
-        
+
         DestroyImmediate(prefab.gameObject, false);
+    }
 
-        AssetDatabase.CreateAsset(storeItem, $"Assets/4_ScriptableObject/{characterName}SO.asset");
-        AssetDatabase.SaveAssets();
+    public void ReimportModel(StoreItem storeItem, Object model)
+    {
+        CreatePrefabForCharacter(storeItem, model);
+    }
 
-        Store.Instance.StoreItems.Add(storeItem);
+    public void ReimportTexture(StoreItem storeItem, Sprite icon)
+    {
+        storeItem.Icon = icon;
+    }
+
+    public void RemoveCharacter(StoreItem storeItem)
+    {
+        Store.Instance.StoreItems.Remove(storeItem);
+        AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(storeItem));
     }
 }
